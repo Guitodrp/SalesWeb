cdd6f712bfe9e530964127eb5ce42f5d1d4ce260
yago <guitoflash@gmail.com>
2024-05-29 09:35:29 -0300

    Entidades e segunda migration

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWeb.Models;
 
 namespace SalesWeb.Data
 {
     public class SalesWebContext : DbContext
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+        public SalesWebContext(DbContextOptions<SalesWebContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWeb.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
