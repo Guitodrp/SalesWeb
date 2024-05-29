14bb965fe7c3567e12437a2a0f239b7e394eee3d
Yago Oliveira <guito_flash@yahoo.com>
2024-05-29 16:43:00 -0300

    #Finalizacao do create

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWeb.Models;
+using SalesWeb.Models.ViewModels;
 using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
@@ -8,10 +9,12 @@ namespace SalesWeb.Controllers
     {
         //Injecao de Dependencia
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -22,7 +25,9 @@ namespace SalesWeb.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();//Buscas todos os departamentos
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
